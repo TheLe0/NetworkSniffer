57cbe21955186f52f899de5eb9a9be31bb156b87
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2020-07-30 15:18:06 -0300

    Correct Namespace

--- NetworkSniffer.Core/IPHandler.cs
+++ NetworkSniffer.Core/IPHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text.RegularExpressions;
 
-namespace NetworkSnifferLib.Logic
+namespace NetworkSniffer.Core
 {
     public class IpHandler
     {
